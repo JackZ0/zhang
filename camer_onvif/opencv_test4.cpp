33fb531a62c64139ee1b0bbe3dff863f8a4146ad
Zhang Hao <2162990048@qq.com>
2018-05-27 15:51:34 +0800

    lab色彩空间转换
    lab色彩空间转换

---
+++ camer_onvif/opencv_test4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>    
+#include <opencv2/highgui/highgui.hpp>    
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>    
+#include <opencv2/ml/ml.hpp>    
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+#include <Windows.h>  
+  
+
+#pragma comment( lib, "opencv_highgui249d.lib")
+#pragma comment(lib,"opencv_imgproc249d.lib")  
+#pragma comment(lib, "opencv_core249.lib")
+#pragma comment(lib,"opencv_contrib249d.lib")
+#pragma comment(lib,"opencv_objdetect249d.lib")   
+#pragma comment(lib,"opencv_photo249d.lib")   
+#pragma comment(lib,"opencv_nonfree249d.lib")   
+#pragma comment(lib,"opencv_features2d249d.lib")   
+
+using namespace std;    
+using namespace cv;    
+
+int main()
+{
+	Mat srcImage = imread("1.jpg",1),dstImage;
+	cvtColor(srcImage,dstImage,COLOR_BGR2Lab);
+	imshow("图",dstImage);
+	waitKey();
+}
