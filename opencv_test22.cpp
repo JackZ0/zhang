1f995d5cecf8c9126c1c574a472d1f9a0ac513f5
Zhang Hao <2162990048@qq.com>
2018-05-29 08:43:30 +0800

    Update opencv_test22.cpp
    找到对应的 角点坐标

--- opencv_test22.cpp
+++ opencv_test22.cpp
@@ -31,9 +31,11 @@
 using namespace cv;  
 using namespace std;
 
+
 #define max_corners 10000 
 int main()
 {
+	int a[10000]={0};
 	int cornerCount=max_corners;
 	CvPoint2D32f corners[max_corners]; 
     IplImage *srcImage = 0, *grayImage = 0, *corners1 = 0, *corners2 = 0; 
@@ -46,18 +48,17 @@ int main()
 	cvCvtColor(srcImage,grayImage,CV_BGR2GRAY);
 	corners1= cvCreateImage(cvGetSize(srcImage), IPL_DEPTH_32F, 1); 
     corners2= cvCreateImage(cvGetSize(srcImage),IPL_DEPTH_32F, 1); 
-	cvGoodFeaturesToTrack (grayImage, corners1,corners2, corners, &cornerCount, 0.05, 
-        5,  
-        0, 
-        3, // block size 
+	cvGoodFeaturesToTrack (grayImage, corners1,corners2, corners, &cornerCount, 0.05,5,0,3, // block size 
         0, // not use harris 
         0.4 ); 
 	printf("num corners found: %d\n", cornerCount);
-	if(cornerCount>0)  
+	if(cornerCount > 0)  
     { 
         for (i=0; i<cornerCount; i++)  
         { 
-            cvCircle(srcImage, cvPoint((int)(corners[i].x), (int)(corners[i].y)), 6, color, 2, CV_AA, 0); \
+            cvCircle(srcImage, cvPoint((int)(corners[i].x), (int)(corners[i].y)), 6, color, 2, CV_AA, 0);
+			a[i]=(int)(corners[i].x)*(int)(corners[i].y)+(int)(corners[i].x);
+			printf("i = %d corners[%d].x= %d  corners[%d].y= %d a[%d] %d\n",i,i, (int)(corners[i].x),i,(int)(corners[i].y),i,a[i]);
 		}
 	 }
 	 cvShowImage( "image", srcImage );  
